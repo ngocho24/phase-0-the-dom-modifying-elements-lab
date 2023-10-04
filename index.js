b39ba1ee7cc013efb2194a417936ca73b2d4be1e
Elijah Ngocho <elijahngocho24@gmail.com>
2023-10-04 14:31:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 // Write your code here!
-const element =  document.createElement("div");
-document.body.append(element);
+// const element =  document.createElement("div");
+// document.body.append(element);
+
+// const main = document.querySelector('#main');
+// main.remove();
+
+// const newHeader = document.createElement('h1');
+// newHeader.id = 'victory';
+// newHeader.innerHTML = 'YOUR-NAME is the champion';
+// document.body.appendChild(newHeader);
+
+
+const headerOne = document.querySelector('h1#victory');
+ headerOne.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion'; 
+document.body.appendChild(newHeader);
